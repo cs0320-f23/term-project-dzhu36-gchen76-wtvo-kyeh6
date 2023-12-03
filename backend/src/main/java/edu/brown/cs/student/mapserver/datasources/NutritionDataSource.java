56da657b3b95faa5a3d040443308af73ecde91bc
Kyle Yeh <kyleyeh@kyles-air-2.devices.brown.edu>
2023-12-02 19:21:16 -0500

    pair-prog w/dzhu36, gchen76: feat: finished API call

--- backend/src/main/java/edu/brown/cs/student/mapserver/datasources/NutritionDataSource.java
+++ backend/src/main/java/edu/brown/cs/student/mapserver/datasources/NutritionDataSource.java
@@ -1,5 +1,6 @@
 package edu.brown.cs.student.mapserver.datasources;
 
+import com.squareup.moshi.Json;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.Types;
@@ -9,8 +10,11 @@ import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import okio.Buffer;
 
 public class NutritionDataSource implements Query<String, String>{
@@ -28,10 +32,9 @@ public class NutritionDataSource implements Query<String, String>{
     HttpURLConnection clientConnection = (HttpURLConnection) requestUrl.openConnection();
     clientConnection.connect();
     List<List<String>> dataList =
-        jsonAdapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
+            jsonAdapter.fromJson(new Buffer().readFrom(clientConnection.getInputStream()));
     List<String> targetRow = dataList.get(1);
-    String broadband = targetRow.get(1);
-    return broadband;
+      return targetRow.get(1);
   }
 
   /**
@@ -47,99 +50,121 @@ public class NutritionDataSource implements Query<String, String>{
     clientConnection.connect();
     if (clientConnection.getResponseCode() != 200) {
       throw new DatasourceException(
-          "unexpected: API connection not success status " + clientConnection.getResponseMessage());
+              "unexpected: API connection not success status " + clientConnection.getResponseMessage());
     }
     return clientConnection;
   }
 
-  /**
-   * Given a state and county (one string), find the broadband percentage at that location by
-   * invoking the US Census ACS API.
-   *
-   * @param stateCounty the state and county as one string to allow for cache loading.
-   * @return the broadband percentage of a given county and state.
-   * @throws DatasourceException if an error is encountered when getting data from the API.
-   */
-  public List<List<String>> getNutritionData(List<String> listFoods) throws DatasourceException {
+  public Map<String, List<FoodNutrient>> getNutritionData(List<String> listFoods) throws DatasourceException {
     try (Buffer buffer = new Buffer()) {
       Moshi moshi = new Moshi.Builder().build();
-      Type listString = Types.newParameterizedType(List.class, List.class, String.class);
-      JsonAdapter<List<List<String>>> adapter = moshi.adapter(listString);
+      JsonAdapter<FoodDataResponse> adapter = moshi.adapter(FoodDataResponse.class);
+      Map<String, List<FoodNutrient>> returnMap = new HashMap<>();
       HttpURLConnection clientConnection;
-      // Find the state code from the state name
-      if (listFoods.size() == 0) {
+      if (listFoods.isEmpty()) {
         throw new DatasourceException("Food list empty");
       }
       for (String food : listFoods) {
-        food = food.replace(" ", "%20");
         URL foodURL =
-            new URL(
-                "https",
-                "api.nal.usda.gov",
-                "/fdc/v1/foods/search?dataType=Foundation&sortBy=dataType.keyword&sortOrder=asc&api_key=" + apikey.getKey() + "&query=" + food);
+                new URL(
+                        "https",
+                        "api.nal.usda.gov",
+                        "/fdc/v1/foods/search?dataType=Foundation" +
+                                "&sortBy=dataType.keyword" +
+                                "&pageSize=1" +
+                                "&sortOrder=asc" +
+                                "&api_key=" + apikey.getKey() +
+                                "&query=" + food.replace(" ", "%20"));
         clientConnection = connect(foodURL);
-      }
-      // find county code from county and state name
-      URL countyCodeURL =
-          new URL(
-              "https",
-              "api.census.gov",
-              "/data/2010/dec/sf1?get=NAME&for=county:*&in=state:" + stateCode);
-      clientConnection = connect(countyCodeURL);
-      List<List<String>> counties =
-          adapter.fromJson(buffer.readFrom(clientConnection.getInputStream()));
-      String countyCode = "-1";
-      for (int i = 1; i < counties.size(); i++) {
-        if (counties.get(i).get(0).equals(stateAndCounty[1] + ", " + stateAndCounty[0])) {
-          countyCode = counties.get(i).get(2);
+        FoodDataResponse response = adapter.fromJson(buffer.readFrom(clientConnection.getInputStream()));
+        if (response == null) {
+          continue;
+        } else if (response.foods == null || response.foods.get(0) == null) {
+          returnMap.put(food, new ArrayList<>());
+        } else {
+          returnMap.put(response.foods.get(0).description, response.foods.get(0).foodNutrients);
         }
+        clientConnection.disconnect();
       }
-      // final request
-      URL requestURL =
-          new URL(
-              "https",
-              "api.census.gov",
-              "/data/2021/acs/acs1/subject/variables?"
-                  + "get=NAME,S2802_C03_022E&for=county:"
-                  + countyCode
-                  + "&in=state:"
-                  + stateCode);
-      clientConnection = connect(requestURL);
-      List<List<String>> body =
-          adapter.fromJson(buffer.readFrom(clientConnection.getInputStream()));
-      clientConnection.disconnect();
-      if (body != null) {
-        body.remove(0);
-      }
-      return body;
+      return returnMap;
     } catch (Exception e) {
       throw new DatasourceException(e.getMessage());
     }
   }
 
   private double calculateBMR(
-      int weight_kg, int height_cm, int age_years, String gender, String activity) {
+          int weight_kg, int height_cm, int age_years, String gender, String activity) {
     double caloricRequirement = 0;
-    if (gender.toLowerCase() == "male") {
+    if (gender.equalsIgnoreCase("male")) {
       caloricRequirement = 10 * weight_kg + 6.25 * height_cm - 5 * age_years + 5;
-    } else if (gender.toLowerCase() == "female") {
+    } else if (gender.equalsIgnoreCase("female")) {
       caloricRequirement = 10 * weight_kg + 6.25 * height_cm - 5 * age_years - 161;
     }
-    switch(activity) {
-      case "Sedentary":
-        return caloricRequirement * 1.2;
-      case "Lightly Active":
-        return caloricRequirement * 1.375;
-      case "Moderately Active":
-        return caloricRequirement * 1.55;
-      case "Very Active":
-        return caloricRequirement * 1.725;
-      case "Extra Active":
-        return caloricRequirement * 1.9;
-      default:
-        return caloricRequirement;
-    }
+      return switch (activity) {
+          case "Sedentary" -> caloricRequirement * 1.2;
+          case "Lightly Active" -> caloricRequirement * 1.375;
+          case "Moderately Active" -> caloricRequirement * 1.55;
+          case "Very Active" -> caloricRequirement * 1.725;
+          case "Extra Active" -> caloricRequirement * 1.9;
+          default -> caloricRequirement;
+      };
+  }
+  public record FoodSearchCriteria(
+          @Json(name = "dataType") List<String> dataType,
+          @Json(name = "query") String query,
+          @Json(name = "generalSearchInput") String generalSearchInput,
+          @Json(name = "pageNumber") int pageNumber,
+          @Json(name = "sortBy") String sortBy,
+          @Json(name = "sortOrder") String sortOrder,
+          @Json(name = "numberOfResultsPerPage") int numberOfResultsPerPage,
+          @Json(name = "pageSize") int pageSize,
+          @Json(name = "requireAllWords") boolean requireAllWords,
+          @Json(name = "foodTypes") List<String> foodTypes
+  ) {
   }
 
+  public record FoodNutrient(
+          @Json(name = "nutrientId") int nutrientId,
+          @Json(name = "nutrientName") String nutrientName,
+          @Json(name = "nutrientNumber") String nutrientNumber,
+          @Json(name = "unitName") String unitName,
+          @Json(name = "derivationCode") String derivationCode,
+          @Json(name = "derivationDescription") String derivationDescription,
+          @Json(name = "derivationId") int derivationId,
+          @Json(name = "value") double value,
+          @Json(name = "foodNutrientSourceId") int foodNutrientSourceId,
+          @Json(name = "foodNutrientSourceCode") String foodNutrientSourceCode,
+          @Json(name = "foodNutrientSourceDescription") String foodNutrientSourceDescription,
+          @Json(name = "rank") int rank,
+          @Json(name = "indentLevel") int indentLevel,
+          @Json(name = "foodNutrientId") int foodNutrientId,
+          @Json(name = "dataPoints") int dataPoints
+  ) {
+  }
 
+  public record Food(
+          @Json(name = "fdcId") int fdcId,
+          @Json(name = "description") String description,
+          @Json(name = "commonNames") String commonNames,
+          @Json(name = "additionalDescriptions") String additionalDescriptions,
+          @Json(name = "dataType") String dataType,
+          @Json(name = "ndbNumber") int ndbNumber,
+          @Json(name = "publishedDate") String publishedDate,
+          @Json(name = "foodCategory") String foodCategory,
+          @Json(name = "allHighlightFields") String allHighlightFields,
+          @Json(name = "score") double score,
+          @Json(name = "microbes") List<String> microbes,
+          @Json(name = "foodNutrients") List<FoodNutrient> foodNutrients
+  ) {
+  }
+
+  public record FoodDataResponse(
+          @Json(name = "totalHits") int totalHits,
+          @Json(name = "currentPage") int currentPage,
+          @Json(name = "totalPages") int totalPages,
+          @Json(name = "pageList") List<Integer> pageList,
+          @Json(name = "foodSearchCriteria") FoodSearchCriteria foodSearchCriteria,
+          @Json(name = "foods") List<Food> foods
+  ) {
+  }
 }
