c5706fe8b0cb4442ac7422cd081dd1e134de5066
Wilson Vo <wilsonvo@wilsons-mbp.devices.brown.edu>
2023-12-04 20:25:20 -0500

    attempted to fix bug. Added new files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
-import grannyImage from './public'
+import grannyImage from './public/old-woman-gardening.avif'
 
 class App extends React.Component {
   render() {
--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -3,41 +3,39 @@
 import React from 'react';
 import logoImage from './public/LogoP.png';
 
-
+ 
 interface HeaderProps {
-onHomeClick: () => void;
-onTeamClick: () => void;
-onUseApplicationClick: () => void;
+
 }
 
+function handleClickHome (){
+  // Logic to handle the "Home" button click
+  console.log('Home button clicked');
+};
 
-class Header extends React.Component<HeaderProps> {
-  handleClickHome = () => {
-    // Logic to handle the "Home" button click
-    console.log('Home button clicked');
-  };
+function handleClickTeam() {
+  // Logic to handle the "Team" button click
+  console.log('Team button clicked');
+};
 
-  handleClickTeam = () => {
-    // Logic to handle the "Team" button click
-    console.log('Team button clicked');
-  };
-
-  handleClickUseApplication = () => {
-    // Logic to handle the "Use Application" button click
-    console.log('Use Application button clicked');
-  };
-  render() {
+function handleClickUseApplication() {
+  // Logic to handle the "Use Application" button click
+  console.log('Use Application button clicked');
+};
+const Header = () => {
+ 
+   {
     return (
       <div className="header">
-        <Image >{logoImage}</Image>
+        <img >src={logoImage}</img>
         <h1>urePlate</h1>
-        <button className="header-button"onClick={this.handleClickHome}>
+        <button className="header-button"onClick={handleClickHome}>
           Home
         </button >
-        <button className="header-button" onClick={this.handleClickTeam}>
+        <button className="header-button" onClick={handleClickTeam}>
           Meet The Team
         </button>
-        <button className="header-button" onClick={this.handleClickUseApplication}>
+        <button className="header-button" onClick={handleClickUseApplication}>
           Use Application!
         </button>
       </div>
